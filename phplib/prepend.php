16ad872b18bff92c3b3b3acce6c3d6ae897f387c
Marcos Marado <mindboosternoori@gmail.com>
2014-04-16 11:52:36 +0100

    Removed the 'initialuser' script, since upstream has a new way of doing it

--- phplib/prepend.php
+++ phplib/prepend.php
@@ -33,7 +33,6 @@ if (array_key_exists("parameters",$_GET)) {
 }
 
 $_ENV["local"] = $_SERVER["DOCUMENT_ROOT"]."/phplib/"; 
-if (php_sapi_name() === 'cli') $_ENV["local"] = getcwd()."/phplib/";
 $_ENV["libdir"] = "/usr/share/phplib/";
 
 $QUERY_STRING="";
