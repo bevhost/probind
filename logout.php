3f99bb4632d6e7423ef2c0d8f35a15f6f868dfb9
David Beveridge <dave@bevhost.com>
2014-04-23 12:21:40 +1000

    delete session variables on logout

--- logout.php
+++ logout.php
@@ -3,6 +3,19 @@ include('phplib/prepend.php');
 include('inc/config.php');
 page_open(array("sess"=>"probind_Session"));
 $widemode="";
+$sess->that->db->Record = false;
+
+foreach($sess->pt as $thing=>$val) {  # forget everything stored in our session
+        switch($thing) {
+                case "cart":    /* except these */
+                case "auth":
+                        break;
+                default:
+                        #echo "<br>Unsetting $thing";
+                        unset($$thing);
+                        unset($sess->pt[$thing]);
+        }
+}
 $auth->logout();
 page_close();
 header("Location:index.php");
