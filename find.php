f728537af9b4ad8c8080d7988b63d4fb4d76068e
David Beveridge <dave@bevhost.com>
2014-05-09 19:16:40 +1000

    ajax backend server function for Linked table Selector

--- find.php
+++ find.php
@@ -9,7 +9,7 @@ include("inc/lib.inc");
 
 $db = new DB_probind;
 
-get_request_values("ZoneExists,ZoneNotExists,SetZone,SrchZone,DomType");
+get_request_values("ZoneExists,ZoneNotExists,SetZone,SrchZone,DomType,Frm,Col,Desc,v,a");
 
 if ($ZoneExists) {
     if (!preg_match("/$DOMAIN_RE_NODOT/", $ZoneExists)) {
@@ -67,5 +67,47 @@ if ($SrchZone!==false) {
 	echo domain_list("%$lookfor%", $DomType, $format);
 }
 
-
+if ($Frm) {
+        echo "<a href=javascript:hidepopup(); id=close title='close'>x</a>\n<br>";
+        $Col = str_replace("_Selector","",$Col);
+        $sql = "SELECT * FROM LinkedTables WHERE FormName=? AND FieldName=? AND LinkDesc=?";
+        $db->prepare($sql);
+        $db->execute($Frm,$Col,$Desc);
+        if ($db->next_record()) {
+                $field = $db->qi($db->Record["LinkField"]);
+                $table = $db->qi($db->Record["LinkTable"]);
+                $sql = "SELECT $field";
+                if ($extra = $db->Record["LinkDesc"]) {
+                        $desc = $db->qi($extra);
+                        $sql .= ", $desc";
+                        $sel = $desc;
+                } else {
+                        $sel = $field;
+                }
+                if ($info = $db->Record["LinkInfo"]) {
+                        $info = $db->qi($info);
+                        $sql .= ", $info";
+                }
+                $sql .= " FROM $table";
+                $sql .= " WHERE $sel LIKE '%$v%'";
+                if ($LinkCondition = $db->Record["LinkCondition"]) {
+                        $sql .= " and " . $LinkCondition;
+                }
+                if ($extra) $sql .= " Order By $desc"; else $sql .= " Order By $field";
+                $sql .= " limit 0,10";
+                $db->query($sql);
+                echo "<ul>\n";
+                while ($db->next_record()) {
+                        $KEY = $db->Record[0];
+                        $VAL = $db->Record[1];
+			if ($a=='ips') $IPS="="; else $IPS="";
+                        echo " <li><a onclick='document.$Frm.$Col.value=\"$KEY\";document.$Frm.${Col}_Selector.value=\"$IPS$VAL\";hidepopup();'>";
+                        if ($extra) echo $VAL; else echo $KEY;
+                        $count = 2;
+                        if ($info) while(array_key_exists($count,$db->Record)) echo ", ".$db->Record[$count++];
+                        echo "</a></li>\n";
+                }
+                echo "</ul>\n";
+        }
+}
 
